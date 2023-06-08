d9b941401904bbc7eeeb2ea4df3ccb562eac7692
Suphadet Vatjanajaroenrat <suphadet.b@gmail.com>
2023-06-08 19:32:51 +0700

    update to one command build

--- server/corsConfig.ts
+++ server/corsConfig.ts
@@ -1,4 +1,4 @@
 export const corsConfig = {
-  origin: ["http://localhost:80", "http://127.0.0.1:5173", "http://127.0.0.1"],
+  origin: ["http://localhost:80", "http://127.0.0.1:5173", "http://127.0.0.1", "http://localhost"],
   methods: ["GET", "PATCH", "PUT", "POST", "DELETE", "OPTIONS"],
 };
